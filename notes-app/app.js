d026d446266af5423c8f8132a1993539f3a3ebea
ryujignh <ryujignh@gmail.com>
2020-01-22 07:55:07 +0900

    Add yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,59 @@
-const validator = require('validator');
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-const msg = getNotes();
-console.log(msg);
+// Customize yargs version
+yargs.version('1.1.0')
 
-console.log(chalk.green('Hello world!'));
-console.log(chalk.red('Hello world!'));
-console.log(chalk.blueBright('Hello world!'));
+// Add note
+yargs.command({
+    command: 'add',
+    describe: 'Add a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string',
+        },
+        body: {
+            describe: 'Body of the note',
+            demandOption: true,
+            type: 'string',
+        },
+    },
+    handler: function (argv) {
+        console.log("Title: " + argv.title);
+        console.log("Body: " + argv.body);
+    }
+});
+
+// Remove note
+yargs.command({
+    command: 'remove',
+    describe: 'remove a note',
+    handler: function () {
+        console.log('removing a note')
+    }
+});
+
+// List note
+yargs.command({
+    command: 'list',
+    describe: 'list a note',
+    handler: function () {
+        console.log('listing a note')
+    }
+});
+
+// Read note
+yargs.command({
+    command: 'read',
+    describe: 'read a note',
+    handler: function () {
+        console.log('reading a note')
+    }
+});
+
+
+// console.log(yargs.argv);
+yargs.parse()
