5847566bf7b1e92d37b152e0a79e52d838e2f82d
ryujignh <ryujignh@gmail.com>
2020-01-21 20:02:55 +0900

    Add chalk

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,8 @@
 const validator = require('validator');
+const chalk = require('chalk');
 const getNotes = require('./notes.js');
 
 const msg = getNotes();
-
 console.log(msg);
 
-console.log(validator.isEmail('andrew@example.com'));
+console.log(chalk.green('Hello world!'));
